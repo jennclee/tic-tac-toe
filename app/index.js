eddd391ba3249c89c0630de42e6309941bfa4db5
jennclee <8753352+jennclee@users.noreply.github.com>
2018-01-08 15:57:14 -0800

    Add prompts for CL app

--- app/index.js
+++ app/index.js
@@ -1,7 +1,19 @@
 const readline = require('readline');
-const Board = require('board.js');
+const Board = require('./board.js');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
 // Start game and create board
+rl.question('Welcome to tic-tac-toe. Please state the board size: ', (size) => {
+  // new Board(size);
+  rl.close();
+});
 
 // Add piece
-  // place coordinates
+rl.question('Where would you like to place your piece? ', (coordinates) => {
+    // Board.togglePiece(coordinates);
+  rl.close();
+});
